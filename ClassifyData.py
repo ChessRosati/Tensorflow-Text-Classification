23b19330ecd1064ee421ed59fbcee4dae07a6b8b
ChessRosati <60305508+ChessRosati@users.noreply.github.com>
2021-01-12 04:32:37 -0500

    Update ClassifyData.py

--- ClassifyData.py
+++ ClassifyData.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+
 data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=100000)
